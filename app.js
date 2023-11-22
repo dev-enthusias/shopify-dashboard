56b884bc342d60fc920d27bb849576d404473319
Anakor Joshua <backend.mern@gmail.com>
2023-11-22 12:04:34 +0100

    Added notifcation menu display functionality

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ closeTrialBtn.addEventListener('click', function () {
   trialComponent.style.display = 'none';
 });
 
-// Toggle profile dropdown
+// Toggle profile menu
 const profileMenuComponent = document.querySelector('.profile--dropdown');
 const profileIcon = document.getElementById('profile');
 profileIcon.addEventListener('click', function () {
   profileMenuComponent.classList.toggle('hidden');
 });
 
-
+// Toggle notification dropdown
+const notificationMenuComponent = document.querySelector(
+  '.notifications--dropdown'
+);
+const notificationIcon = document.getElementById('notification');
+notificationIcon.addEventListener('click', function () {
+  notificationMenuComponent.classList.toggle('hidden');
+});
