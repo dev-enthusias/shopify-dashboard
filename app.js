b16c8b21db84231e95a5b290342572fdd1244991
Anakor Joshua <backend.mern@gmail.com>
2023-11-22 11:31:31 +0100

    Added profile menu display functionality

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ closeTrialBtn.addEventListener('click', function () {
   trialComponent.style.display = 'none';
 });
 
+// Toggle profile dropdown
+const profileMenuComponent = document.querySelector('.profile--dropdown');
+const profileIcon = document.getElementById('profile');
+profileIcon.addEventListener('click', function () {
+  profileMenuComponent.classList.toggle('hidden');
+});
+
 
